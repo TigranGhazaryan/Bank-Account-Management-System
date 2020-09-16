f7ffdb90503a61a4158aaff8e5c358c94ce0ec11
Tigran <tigran.ghazaryan@outlook.com>
2020-09-16 16:48:51 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,24 +11,24 @@ int main()
     json k;
     Customer_Accounts C;
     C.Main_Page();
-    int Input{};
+    std::string Input{};
     while (std::cin >> Input)
     {
-        if (Input > 7 || Input < 1)
-            std::cout << "Wrong Input, Try again\n";
-        if (Input == 1)
+        if (Input == "1")
             C.Create_Account(k, j);
-        if (Input == 2)
+        else if (Input == "2")
             C.Update_Existing_Account(k);
-        if (Input == 3)
+        else if (Input == "3")
             C.For_Transactions(k);
-        if (Input == 4)
+        else if (Input == "4")
             C.Check_Existing_Account(k);
-        if (Input == 5)
+        else  if (Input == "5")
             C.Remove_Existing_Account(k);
-        if (Input == 6)
+        else  if (Input == "6")
             C.Print_Customer_List(k);
-        if (Input == 7)
+        else if (Input == "7")
             break;
+        else
+            std::cout << "Wrong Input, Try again\n";
     }
 }
